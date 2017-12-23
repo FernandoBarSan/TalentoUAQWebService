4677ef94bea6a99a3ac48342fec06794beaf3317
Neflow <Neflow@PCWIN10X64>
2017-12-23 09:45:12 -0600

    cambios

---
+++ TalentoAUQService/Controllers/tblestadosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TalentoAUQService.Models;
+
+namespace TalentoUAQWebService.Controllers
+{
+    public class tblestadosController : ApiController
+    {
+        // GET: api/tblestados
+        [Route("api/tblestados")]
+        public HttpResponseMessage Get()
+        {
+            var subcategorias = TalentoRepository.GetEstado();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, subcategorias);
+            return response;
+        }
+
+        // GET: api/tblestados/5
+        [Route("api/tblestados/{id?}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var subcategoria = TalentoRepository.GetEstadoById(id);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, subcategoria);
+            return response;
+        }
+
+        // POST: api/tblestados
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/tblestados/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/tblestados/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TalentoAUQService/Models/TalentoRepository.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalentoAUQService.Models
+{
+    public class TalentoRepository
+    {
+        private static TalentoUAQEntities dataContext = new TalentoUAQEntities();
+        public static List<tblcategoria> GetCategorias()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+
+            var query = from tblcategoria in dataContext.tblcategorias
+                        select tblcategoria;
+            return query.ToList();
+        }
+
+        public static tblcategoria GetCategoriaById(int cveCategoria)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblcategorias in dataContext.tblcategorias
+                        where tblcategorias.cveCategoria == cveCategoria
+                        select tblcategorias;
+            return query.SingleOrDefault();
+        }
+
+        public static List<tblsubcategoria> GetSubcategoria()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblsubcategorias in dataContext.tblsubcategorias
+                        select tblsubcategorias;
+            return query.ToList();
+        }
+
+        public static tblsubcategoria GetSubcategoriaById(int cveSubcategoria)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblsubcategorias in dataContext.tblsubcategorias
+                        where tblsubcategorias.cveSubcategoria == cveSubcategoria
+                        select tblsubcategorias;
+            return query.SingleOrDefault();
+        }
+
+        public static List<tblsubcategoria> GetSubcategoriaByCategoria(int cveCategoria)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblsubcategorias in dataContext.tblsubcategorias
+                        where tblsubcategorias.cveCategoria == cveCategoria
+                        select tblsubcategorias;
+            return query.ToList();
+        }
+
+        public static List<tblestado> GetEstado()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblestados in dataContext.tblestados
+                        select tblestados;
+            return query.ToList();
+        }
+
+        public static List<tblestado> GetEstadoById(int cveEstado)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblestados in dataContext.tblestados
+                        where tblestados.cveEstado == cveEstado
+                        select tblestados;
+            return query.ToList();
+        }
+
+        public static List<tblestado> GetMunicipio()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblestados in dataContext.tblestados
+                        select tblestados;
+            return query.ToList();
+        }
+
+        public static List<tblmunicipio> GetMunicipioById(int cveMunicipio)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblmunicipios in dataContext.tblmunicipios
+                        where tblmunicipios.cveMunicipio == cveMunicipio
+                        select tblmunicipios;
+            return query.ToList();
+        }
+
+        public static List<tblmunicipio> GetMunicipioByIdEstado(int cveEstado)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblmunicipios in dataContext.tblmunicipios
+                        where tblmunicipios.cveEstado == cveEstado
+                        select tblmunicipios;
+            return query.ToList();
+        }
+
+        public static List<tblempresa> GetEmpresa()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblempresas in dataContext.tblempresas
+                        select tblempresas;
+            return query.ToList();
+        }
+
+        public static List<tblempresa> GetEmpresaById(int cveEmpresa)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblempresas in dataContext.tblempresas
+                        where tblempresas.cveEmpresa == cveEmpresa
+                        select tblempresas;
+            return query.ToList();
+        }
+
+        public static List<tblfavorito> GetFavorito()
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblfavoritos in dataContext.tblfavoritos
+                        select tblfavoritos;
+            return query.ToList();
+        }
+
+        public static List<tblfavorito> GetFavoritoById(int idFavorito)
+        {
+            dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tblfavoritos in dataContext.tblfavoritos
+                        where tblfavoritos.idFavorito == idFavorito
+                        select tblfavoritos;
+            return query.ToList();
+        }
+
+    }
+}
