11f51fd43c5e786e530496b141f1385d64e2647b
Neflow <Neflow@PCWIN10X64>
2018-01-08 17:45:02 -0600

    webservices

--- TalentoUAQWebService/Models/TalentoRepository.cs
+++ TalentoUAQWebService/Models/TalentoRepository.cs
@@ -117,70 +117,160 @@ namespace TalentoUAQWebService.Models
             return query.ToList();
         }
 
-        public static List<tbloferta> GetFavorito()
+        public static List<OfertaResult> GetFavorito()
         {
-            favoritosClass favorit = new favoritosClass();
-            dataContext.Configuration.LazyLoadingEnabled = false;
+            //dataContext.Configuration.LazyLoadingEnabled = false;
             var query = from tbloferta in dataContext.tblofertas
                         join tblfavoritos in dataContext.tblfavoritos on tbloferta.idOferta equals tblfavoritos.idOferta
                         join tblsubcategoria in dataContext.tblsubcategorias on tbloferta.cveSubcategoria equals tblsubcategoria.cveSubcategoria
+                        join tblmunicipio in dataContext.tblmunicipios on tbloferta.cveMunicipio equals tblmunicipio.cveMunicipio
+                        join tblestado in dataContext.tblestados on tblmunicipio.cveEstado equals tblestado.cveEstado
+                        join tblcategoria in dataContext.tblcategorias on tblsubcategoria.cveCategoria equals tblcategoria.cveCategoria
+                        join tbltiposempleo in dataContext.tbltiposempleos on tbloferta.cveTipoEmpleo equals tbltiposempleo.cveTipoEmpleo
+                        join tblempresa in dataContext.tblempresas on tbloferta.cveEmpresa equals tblempresa.cveEmpresa 
                         where tbloferta.activo=="S" && tblfavoritos.activo=="S"
-                        select tblfavoritos.tbloferta as tbloferta;
+                        select tbloferta ;
             query.Where(a => a.activo == "S");
-            favorit.oferta = query.ToList();
-            return query.ToList();
+
+            var lista = query.ToList();
+            var listaResult = new List<OfertaResult>();
+                foreach (tbloferta element in lista)
+                {
+                    var nombreDos = element.tblempresa;
+                    listaResult.Add(new OfertaResult
+                    {
+                        idOferta = element.idOferta.ToString(),
+                        titulo = element.titulo.ToString(),
+                        descripcion = element.descripcion.ToString(),
+                        sueldoInicio = element.sueldoInicio.ToString(),
+                        sueldoFin = element.sueldoFin.ToString(),
+                        fechaInicioOferta = element.fechaInicioOferta.ToString(),
+                        fechaFinOferta = element.fechaFinOferta.ToString(),
+                        cveEmpresa = element.cveEmpresa.ToString(),
+                        nombreEmpresa = element.tblempresa.nombre.ToString(),
+                        nombreContacto = element.nombreContacto.ToString(),
+                        correoContacto = element.correoContacto.ToString(),
+                        telefonoContacto = element.telefonoContacto.ToString(),
+                        cveTipoEmpleo = element.cveTipoEmpleo.ToString(),
+                        descTipoEmpleo = element.tbltiposempleo.descTipoEmpleo.ToString(),
+                        cveSubcategoria = element.cveSubcategoria.ToString(),
+                        descSubcategoria = element.tblsubcategoria.descSubcategoria.ToString(),
+                        cveCategoria = element.tblsubcategoria.cveCategoria.ToString(),
+                        descCategoria = element.tblsubcategoria.tblcategoria.descCategoria.ToString(),
+                        cveMunicipio = element.cveMunicipio.ToString(),
+                        descMunicipio = element.tblmunicipio.descMunicipio.ToString(),
+                        cveEstado = element.tblmunicipio.cveEstado.ToString(),
+                        descEstado = element.tblmunicipio.tblestado.descEstado.ToString(),
+
+                    });
+                }
+            
+            
+            return listaResult;
         }
 
-        public static List<tbloferta> GetFavoritoById(int idUsuarioExterno)
+        public static List<OfertaResult> GetFavoritoById(int idUsuarioExterno)
         {
-            favoritosClass favorit = new favoritosClass();
-            dataContext.Configuration.LazyLoadingEnabled = false;
-            var query = from tbloferta in dataContext.tblofertas join tblfavoritos in dataContext.tblfavoritos on  tbloferta.idOferta equals tblfavoritos.idOferta 
+            //dataContext.Configuration.LazyLoadingEnabled = false;
+            var query = from tbloferta in dataContext.tblofertas join tblfavoritos in dataContext.tblfavoritos on  tbloferta.idOferta equals tblfavoritos.idOferta
+                        join tblmunicipio in dataContext.tblmunicipios on tbloferta.cveMunicipio equals tblmunicipio.cveMunicipio
+                        join tblestado in dataContext.tblestados on tblmunicipio.cveEstado equals tblestado.cveEstado
+                        join tblsubcategoria in dataContext.tblsubcategorias on tbloferta.cveSubcategoria equals tblsubcategoria.cveSubcategoria
+                        join tblcategoria in dataContext.tblcategorias on tblsubcategoria.cveCategoria equals tblcategoria.cveCategoria
+                        join tbltiposempleo in dataContext.tbltiposempleos on tbloferta.cveTipoEmpleo equals tbltiposempleo.cveTipoEmpleo
+                        join tblempresa in dataContext.tblempresas on tbloferta.cveEmpresa equals tblempresa.cveEmpresa
                         where tblfavoritos.idUsuarioExterno == idUsuarioExterno && tblfavoritos.activo == "S" && tbloferta.fechaFinOferta >= DateTime.Today && tbloferta.activo == "S"
                         select tbloferta ;
+            var lista = query.ToList();
+            var listaResult = new List<OfertaResult>();
+            foreach (tbloferta element in lista)
+            {
+                var nombreDos = element.tblempresa;
+                listaResult.Add(new OfertaResult
+                {
+                    idOferta = element.idOferta.ToString(),
+                    titulo = element.titulo.ToString(),
+                    descripcion = element.descripcion.ToString(),
+                    sueldoInicio = element.sueldoInicio.ToString(),
+                    sueldoFin = element.sueldoFin.ToString(),
+                    fechaInicioOferta = element.fechaInicioOferta.ToString(),
+                    fechaFinOferta = element.fechaFinOferta.ToString(),
+                    cveEmpresa = element.cveEmpresa.ToString(),
+                    nombreEmpresa = element.tblempresa.nombre.ToString(),
+                    nombreContacto = element.nombreContacto.ToString(),
+                    correoContacto = element.correoContacto.ToString(),
+                    telefonoContacto = element.telefonoContacto.ToString(),
+                    cveTipoEmpleo = element.cveTipoEmpleo.ToString(),
+                    descTipoEmpleo = element.tbltiposempleo.descTipoEmpleo.ToString(),
+                    cveSubcategoria = element.cveSubcategoria.ToString(),
+                    descSubcategoria = element.tblsubcategoria.descSubcategoria.ToString(),
+                    cveCategoria = element.tblsubcategoria.cveCategoria.ToString(),
+                    descCategoria = element.tblsubcategoria.tblcategoria.descCategoria.ToString(),
+                    cveMunicipio = element.cveMunicipio.ToString(),
+                    descMunicipio = element.tblmunicipio.descMunicipio.ToString(),
+                    cveEstado = element.tblmunicipio.cveEstado.ToString(),
+                    descEstado = element.tblmunicipio.tblestado.descEstado.ToString(),
 
-            favorit.oferta = query.ToList();
-            return query.ToList();
+                });
+            }
+            return listaResult;
         }
 
-        public static List<tbloferta> GetSugerenciasById(int idUsuarioExterno)
+        public static List<OfertaResult> GetSugerenciasById(int idUsuarioExterno)
         {
-            favoritosClass favorit = new favoritosClass();
-            dataContext.Configuration.LazyLoadingEnabled = false;
+            //dataContext.Configuration.LazyLoadingEnabled = false;
             var query = from tblsubcategoriasusuario in dataContext.tblsubcategoriasusuarios
                         join tblsubcategoria in dataContext.tblsubcategorias on tblsubcategoriasusuario.cveSubcategoria equals tblsubcategoria.cveSubcategoria
                         join tbloferta in dataContext.tblofertas on tblsubcategoria.cveSubcategoria equals tbloferta.cveSubcategoria
+                        join tblmunicipio in dataContext.tblmunicipios on tbloferta.cveMunicipio equals tblmunicipio.cveMunicipio
+                         join tblestado in dataContext.tblestados on tblmunicipio.cveEstado equals tblestado.cveEstado
+                         join tblcategoria in dataContext.tblcategorias on tblsubcategoria.cveCategoria equals tblcategoria.cveCategoria
+                         join tbltiposempleo in dataContext.tbltiposempleos on tbloferta.cveTipoEmpleo equals tbltiposempleo.cveTipoEmpleo
+                         join tblempresa in dataContext.tblempresas on tbloferta.cveEmpresa equals tblempresa.cveEmpresa
                         where tblsubcategoriasusuario.idUsuarioExterno == idUsuarioExterno && tbloferta.activo == "S" && tbloferta.fechaFinOferta >= DateTime.Today && tblsubcategoria.activo == "S" && tblsubcategoriasusuario.activo=="S"
                         select tbloferta;
 
-            favorit.oferta = query.ToList();
-            return query.ToList();
+            var lista = query.ToList();
+            var listaResult = new List<OfertaResult>();
+            foreach (tbloferta element in lista)
+            {
+                var nombreDos = element.tblempresa;
+                listaResult.Add(new OfertaResult
+                {
+                    idOferta = element.idOferta.ToString(),
+                    titulo = element.titulo.ToString(),
+                    descripcion = element.descripcion.ToString(),
+                    sueldoInicio = element.sueldoInicio.ToString(),
+                    sueldoFin = element.sueldoFin.ToString(),
+                    fechaInicioOferta = element.fechaInicioOferta.ToString(),
+                    fechaFinOferta = element.fechaFinOferta.ToString(),
+                    cveEmpresa = element.cveEmpresa.ToString(),
+                    nombreEmpresa = element.tblempresa.nombre.ToString(),
+                    nombreContacto = element.nombreContacto.ToString(),
+                    correoContacto = element.correoContacto.ToString(),
+                    telefonoContacto = element.telefonoContacto.ToString(),
+                    cveTipoEmpleo = element.cveTipoEmpleo.ToString(),
+                    descTipoEmpleo = element.tbltiposempleo.descTipoEmpleo.ToString(),
+                    cveSubcategoria = element.cveSubcategoria.ToString(),
+                    descSubcategoria = element.tblsubcategoria.descSubcategoria.ToString(),
+                    cveCategoria = element.tblsubcategoria.cveCategoria.ToString(),
+                    descCategoria = element.tblsubcategoria.tblcategoria.descCategoria.ToString(),
+                    cveMunicipio = element.cveMunicipio.ToString(),
+                    descMunicipio = element.tblmunicipio.descMunicipio.ToString(),
+                    cveEstado = element.tblmunicipio.cveEstado.ToString(),
+                    descEstado = element.tblmunicipio.tblestado.descEstado.ToString(),
+
+                });
+            }
+            return listaResult;
         }
 
-        public static DatosGenerales GetDatosGeneralesById(int idUsuarioExterno)
+        public static List<tblaspirante> GetDatosGeneralesById(int idUsuarioExterno)
         {
-            DatosGenerales DatosG = new DatosGenerales();
-            dataContext.Configuration.LazyLoadingEnabled = false;
             var query = from tblaspirante in dataContext.tblaspirantes
                         where tblaspirante.idUsuarioExterno == idUsuarioExterno && tblaspirante.activo == "S" 
                         select tblaspirante;
-
-            DatosG.aspirante = query.ToList();
-            foreach (tblaspirante aspirante in DatosG.aspirante) {
-                var query2 = from tblexperiencia in dataContext.tblexperiencias
-                            where tblexperiencia.idAspirante == aspirante.idAspirante && tblexperiencia.activo == "S"
-                            select tblexperiencia;
-                DatosG.experiencias = query2.ToList();
-                var query3 = from tblescolaridade in dataContext.tblescolaridades
-                             where tblescolaridade.idAspirante == aspirante.idAspirante && tblescolaridade.activo == "S"
-                             select tblescolaridade;
-                DatosG.escoladirades = query3.ToList();
-                var query4 = from tblidioma in dataContext.tblidiomas
-                             where tblidioma.idAspirante == aspirante.idAspirante && tblidioma.activo == "S"
-                             select tblidioma;
-                DatosG.idiomas = query4.ToList();
-            }
-            return DatosG;
+            return query.ToList();
         }
 
 
